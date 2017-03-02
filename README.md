33eceeff8242f721870ce5c3ae666c031ba6c68c
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-02 02:12:42 +0200

    Create basic RSS reader. Save RSS to DB

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
+- [http://www.doctrine-project.org/projects/dbal.html](Doctrine DBAL)
 - [https://github.com/nilportugues/php-sql-query-builder](nilportugues/php-sql-query-builder)
 - [https://github.com/usmanhalalit/pixie](usmanhalalit/pixie)
+
+- [https://packagist.org/packages/simplepie/simplepie](simplepie/simplepie)
--- rss-reader.php
+++ rss-reader.php
@@ -1,2 +1,24 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+$feed = new SimplePie();
+$feed->enable_cache(false);
+$feed->set_feed_url('https://rss.unian.net/site/news_ukr.rss');
+$feed->init();
+
+
+$db = new PDO("mysql:host=localhost;dbname=rss_news;charset=utf8", "root", "123");
+$sql = "INSERT INTO news (title, link, description, source) VALUES (?, ?, ?, ?)";
+
+$items = $feed->get_items();
+
+foreach ($items as $item) {
+    $stmt = $db->prepare($sql);
+    $stmt->execute([
+        $item->get_title(),
+        $item->get_link(),
+        $item->get_description(),
+        $feed->get_link(),
+    ]);
+}
