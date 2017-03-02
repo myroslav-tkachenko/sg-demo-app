2c70355ed89a38a322fd0a906d63e28a323f9c8e
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-02 03:32:07 +0200

    Handle multiple feeds by hand, consider to improve

--- rss-reader.php
+++ rss-reader.php
@@ -4,24 +4,33 @@ require_once 'vendor/autoload.php';
 
 date_default_timezone_set('Europe/Kiev');
 
-$feed = new SimplePie();
-$feed->enable_cache(false);
-$feed->enable_order_by_date(false);
-$feed->set_feed_url('https://rss.unian.net/site/news_ukr.rss');
-$feed->init();
-
 $db = new PDO("mysql:host=localhost;dbname=rss_news;charset=utf8", "root", "123");
 $sql = "INSERT INTO news (title, link, description, source, pub_date) VALUES (?, ?, ?, ?, ?)";
 $stmt = $db->prepare($sql);
 
-$items = $feed->get_items();
+$feed_urls = [
+    'http://www.pravda.com.ua/rss/',
+    'http://football.ua/rss2.ashx',
+    'https://rss.unian.net/site/news_ukr.rss',
+];
 
-foreach ($items as $item) {
-    $stmt->execute([
-        $item->get_title(),
-        $item->get_link(),
-        $item->get_description(),
-        $feed->get_link(),
-        $item->get_date("Y-m-d H:i:s"),
-    ]);
+$feed = new SimplePie();
+$feed->enable_cache(false);
+$feed->enable_order_by_date(false);
+
+foreach ($feed_urls as $feed_url) {
+    $feed->set_feed_url($feed_url);
+    $feed->init();
+
+    $items = $feed->get_items();
+
+    foreach ($items as $item) {
+        $stmt->execute([
+            $item->get_title(),
+            $item->get_link(),
+            $item->get_description(),
+            $feed->get_link(),
+            $item->get_date("Y-m-d H:i:s"),
+        ]);
+    }
 }
