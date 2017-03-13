e0283afe256c61b1593ac79b55fc082c42b81299
Myroslav Tkachenko <myroslav.tkachenko@gmail.com>
2017-03-13 20:58:36 +0200

    Add HttpFoundation Response

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 require_once '../vendor/autoload.php';
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Session\Session;
 
 use Symfony\Component\Routing\Matcher\UrlMatcher;
@@ -12,19 +14,21 @@ use Symfony\Component\Routing\Route;
 
 // $_SERVER['REQUEST_URI'] = preg_replace('|/$|', '', $_SERVER['REQUEST_URI'], 1);
 $request = Request::createFromGlobals();
-$session = new Session();
+$response = new Response;
+
+$session = new Session;
 $session->start();
 
 $request->setSession($session);
 
 $routes = new RouteCollection();
 
-$routes->add('get_login', new Route('/login', array('_controller' => function ($request) {
-    echo '<form action="/login" method="POST">
+$routes->add('get_login', new Route('/login', array('_controller' => function ($request, $response) {
+    return $response->setContent('<form action="/login" method="POST">
         <input name="name">
         <input name="pass">
         <input type="submit">
-    </form>';
+    </form>');
 }), array(), array(), '', array(), array('GET')));
 
 $routes->add('post_login', new Route('/login', array('_controller' => function ($request) {
@@ -34,35 +38,42 @@ $routes->add('post_login', new Route('/login', array('_controller' => function (
     $session = $request->getSession();
     if ($login == 'test' && $pass == '123') {
         $session->set('logged', true);
+        return new RedirectResponse('/cabinet');
     }
+
+    return new RedirectResponse('/login');
 }), array(), array(), '', array(), array('POST')));
 
-$routes->add('logout', new Route('/logout', array('_controller' => function ($request) {
-
+$routes->add('logout', new Route('/logout', array('_controller' => function ($request, $response) {
     $session = $request->getSession();
     $session->invalidate();
-    echo 'LOGOUT';
+
+    return new RedirectResponse('/');
 }), array(), array(), '', array(), array('GET')));
 
-$routes->add('cabinet', new Route('/cabinet', array('_controller' => function ($request) {
-
+$routes->add('cabinet', new Route('/cabinet', array('_controller' => function ($request, $response) {
     // Check if user was set in Session
     $logged = $request->getSession()->get('logged');
     if ($logged) {
-        echo 'CABINET';
+        $response->setContent('CABINET');
     } else {
-        echo '403';
+        $response->setStatusCode('403');
+        $response->setContent('Forbidden.');
     }
+
+    return $response;
 }), array(), array(), '', array(), array('GET')));
 
-$routes->add('front_route', new Route('/', array('_controller' => function () {
+$routes->add('front_route', new Route('/', array('_controller' => function ($request, $response) {
 
     $db = new PDO("mysql:host=localhost;dbname=rss_news;charset=utf8", "root", "123");
     $sql = "SELECT * FROM news ORDER BY id DESC LIMIT 50";
     $stmt = $db->prepare($sql);
     $stmt->execute();
     $items = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    include '../templates/index.tpl.php';
+
+    $response->setContent(include '../templates/index.tpl.php');
+    return $response;
 })));
 
 $context = new RequestContext();
@@ -72,13 +83,18 @@ $matcher = new UrlMatcher($routes, $context);
 
 try {
     $parameters = $matcher->matchRequest($request);
+    $action = $parameters['_controller'];
 } catch (Symfony\Component\Routing\Exception\ResourceNotFoundException $e) {
-    echo '404';
-    die();
+    $response->setStatusCode('404');
+    $response->setContent('404: Page not found');
 }
 
-$action = $parameters['_controller'];
 
-if (is_callable($action)) {
-    $action($request);
+if (isset($action) && is_callable($action)) {
+    $response = $action($request, $response);
+} else {
+    $response->setStatusCode('404');
+    $response->setContent('404: Page not found');
 }
+
+$response->send();
